44593dfce24f86b8d12bed7ff25969d2189a7f44
Chetan Pai <51903270+chetanpai23@users.noreply.github.com>
2019-10-23 12:49:34 +0530

    Update index.js
    removed the Vuetify-datetime-picker

--- demo/index.js
+++ demo/index.js
@@ -8,7 +8,7 @@ import VueAxios from 'vue-axios'
 import Swatches from 'vue-swatches'
 import 'vue-swatches/dist/vue-swatches.min.css'
 import { Sketch } from 'vue-color'
-import DatetimePicker from 'vuetify-datetime-picker'
+
 
 Vue.use(Vuetify)
 Vue.use(VueAxios, axios)
@@ -16,7 +16,7 @@ Vue.use(VueAxios, axios)
 Vue.component('swatches', Swatches)
 Vue.component('draggable', Draggable)
 Vue.component('color-picker', Sketch)
-Vue.use(DatetimePicker)
+
 
 new Vue({
   el: '#app',
